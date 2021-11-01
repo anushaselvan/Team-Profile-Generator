1f1a85a9b2d3b7f30616b26ae411696be40511a0
Anusha <anushaselvan@gmail.com>
2021-11-02 10:17:34 +1100

    HTML code clean up-added CSS and icons

--- src/generateHtmlPage.js
+++ src/generateHtmlPage.js
@@ -1,84 +1,76 @@
 function addManager(manager){
-  return `<div class= "col-4 mb-3">
-  <div class="card" style="width: 18rem">
-  <div class="card-title bg-info ">
-      <h5>${manager.name}</h5>
-      <p class="sub-title"> Manager</p>
-  </div>
-  <ul class="card-body">
-    <p class="card-text">Id:${manager.id}</p>
-    <p class="card-text">Email:<a href="mailto:${manager.email}">${manager.email}</a> </p>
-    <p class="card-text">Office Number:${manager.officenumber}</p>
-  </ul>       
+  return `
+<div class= "col-4 mb-3">
+<div class="card shadow-lg rounded" style="width: 18rem">
+<div class="card-title bg-info ">
+    <h3>${manager.name}</h3>
+    <h5 class="sub-title"><img src="./assets/Coffee.png" alt="Coffee"/> Manager</h5>
+</div>
+<ul class="card-body">
+    <h6 class="card-text">ID:${manager.id}</h6>
+    <h6 class="card-text">Email:<a href="mailto:${manager.email}">${manager.email}</a> </h6>
+    <h6 class="card-text">Office Number:${manager.officenumber}</h6>
+</ul>       
 </div>
 </div>`;
 }
 
 function addEngineer(engineer){
-  return `<div class= "col-4 mb-3">
-  <div class="card " style="width: 18rem">
-  <div class="card-title bg-info ">
-      <h5>${engineer.name}</h5>
-      <p class="sub-title"> Engineer</p>
-  </div>
-  <ul class=" card-body">
-    <p class="card-text">Id:${engineer.id}</p>
-    <p class="card-text">Email:<a href="mailto:${engineer.email}">${engineer.email}</a> </p>
-    <p class="card-text">GitHub:<a href="https://github.com/${engineer.email}">${engineer.email}</a></p>
-  </ul>       
+  return `
+<div class= "col-4 mb-3">
+<div class="card shadow-lg rounded " style="width: 18rem">
+<div class="card-title bg-info  ">
+    <h3>${engineer.name}</h3>
+    <h5 class="sub-title"><img src="./assets/Glasses.png" alt="Glasses"/> Engineer</h5>
+</div>
+<ul class=" card-body">
+    <h6 class="card-text">ID:${engineer.id}</h6>
+    <h6 class="card-text">Email:<a href="mailto:${engineer.email}">${engineer.email}</a> </h6>
+    <h6 class="card-text">GitHub:<a href="https://github.com/${engineer.github}">${engineer.github}</a></h6>
+</ul>       
 </div>
 </div>`;
 }
 
 function addIntern(intern){
-  return `<div class= "col-4 mb-3">
-  <div class="card " style="width: 18rem">
-  <div class="card-title bg-info ">
-      <h5>${intern.name}</h5>
-      <p class="sub-title"> Intern</p>
+  return `
+  <div class= "col-4 mb-3">
+  <div class="card shadow-lg rounded " style="width: 18rem">
+  <div class="card-title bg-info  ">
+      <h3>${intern.name}</h3>
+      <h5 class="sub-title"><img src="./assets/Intern.png" alt="Intern"/> Intern</h5>
   </div>
   <ul class=" card-body">
-    <p class="card-text">Id:${intern.id}</p>
-    <p class="card-text">Email:<a href="mailto:${intern.email}">${intern.email}</a> </p>
-    <p class="card-text">School:${intern.school}</p>
+      <h6 class="card-text">ID:${intern.id}</h6>
+      <h6 class="card-text">Email:<a href="mailto:${intern.email}">${intern.email}</a> </h6>
+      <h6 class="card-text">School:${intern.school}</h6>
   </ul>       
-</div>
-</div>`;
+  </div>
+  </div>`;
 }
 
 function generateHtmlPage(teamMemberArray) {
- cardArray = [];
- console.log(teamMemberArray);
-for(let i = 0; i < teamMemberArray.length; i++){
-  console.log("Array length is "+ teamMemberArray.length);
- if(teamMemberArray[i].getRole() === "Manager")
- {
-  console.log("I m in manager");
-
-     const managerCard = addManager(teamMemberArray[i]);
-     cardArray.push(managerCard);
- }
- else if(teamMemberArray[i].getRole() === "Engineer")
- {
-  console.log("I m in engineer");
-
-     const engineerCard = addEngineer(teamMemberArray[i]);
-     cardArray.push(engineerCard);
- }
- else
- {
-  console.log("I m in intern");
-
-     const internCard = addIntern(teamMemberArray[i]);
-     cardArray.push(internCard);
- }}
+cardArray = [];
+    for(let i = 0; i < teamMemberArray.length; i++){
+        if(teamMemberArray[i].getRole() === "Manager")
+        {
+            const managerCard = addManager(teamMemberArray[i]);
+            cardArray.push(managerCard);
+        }
+        else if(teamMemberArray[i].getRole() === "Engineer")
+        {
+            const engineerCard = addEngineer(teamMemberArray[i]);
+            cardArray.push(engineerCard);
+        }
+        else
+        {
+            const internCard = addIntern(teamMemberArray[i]);
+            cardArray.push(internCard);
+        }
+    }
  const employeecardArray = cardArray.join('');
- console.log(employeecardArray);
  return generateHTML(employeecardArray);
- console.log(generateHTML(employeecardArray));
 }
-
-
 
 function generateHTML(employeeCardArray){
   return `<!DOCTYPE html>
@@ -93,19 +85,17 @@ function generateHTML(employeeCardArray){
       <title>Team Profile Generator</title>
   </head>
   <body>
-  <header class= "jumbotron bg-info height-200px text-center"><h1>Team Profile</h1></header>
-  <div class="container w-80% ">
-      <section class="row justify-content-center">
-   
-      ${employeeCardArray}
-     
-      </section>
+      <header class= "jumbotron bg-info height-200px text-center"> 
+        <span style = "font-size:4rem; font-weight: bold; font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;">Team Profile</span>
+      </header>
+      <div class="container w-80% ">
+          <section class="row justify-content-center">
+          ${employeeCardArray}
+          </section>
       </div>
   </body>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  
   </html>`;  
 }
-
 
 module.exports = generateHtmlPage;
